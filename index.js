2204a8a028e8e676e8d60582c95ab9fa6bc076c9
pitamchandra <pitamchandra402@gmail.com>
2023-05-18 23:52:24 +0600

    get specific data done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -43,6 +43,15 @@ async function run() {
     // get all toys
     app.get('/allToys', async(req, res)=>{
         const result = await toysCollection.find().limit(20).toArray()
+        res.send(result)
+    })
+
+    // get a specific toy
+    app.get('/allToys/:id', async(req, res) =>{
+        const id = req.params.id;
+        console.log(id);
+        const query = {_id: new ObjectId(id)}
+        const result = await toysCollection.findOne(query)
         res.send(result)
     })
 
